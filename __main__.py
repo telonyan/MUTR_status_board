bb0abe6b28d17d2e629f91de9fef0ba1e17eaed1
Tellawn <yan.telon@gmail.com>
2020-06-24 17:01:52 -0400

    Separated new code into multiple modules

--- __main__.py
+++ __main__.py
@@ -5,18 +5,14 @@ An interactive status board for the Maryland University Training Reactor (MUTR)
 Tries to follow standard code style: https://www.python.org/dev/peps/pep-0008
 
 Created on Tue Jun 23 10:57:39 2020
-@author: Telon Yan
+@author: Telon J. Yan
 """
 # %% Imports
 import tkinter as tk
 
-#Import other user-defined pages
-
-
-# %% Defining constants
-LARGE_FONT = ("Verdana", 12)
-MEDIUM_FONT = ("Verdana", 10)
-SMALL_FONT = ("Verdana", 8)
+#Import user-defined pages
+import start_page
+import test_page
 
 
 # %% Main Tkinter class
@@ -26,16 +22,21 @@ class StatusBoard(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         
+        #Define the fonts we want to use
+        self.LARGE_FONT = ("Verdana", 12)
+        self.MEDIUM_FONT = ("Verdana", 10)
+        self.SMALL_FONT = ("Verdana", 8)
+        
         container = tk.Frame(self)
         
+        container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
         #Dictionary of string frame names to Frame instances
         self.frames = {}
-            
         #Populate self.frames
-        for F in (StartPage):
+        for F in (start_page.StartPage, test_page.TestPage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -43,23 +44,15 @@ class StatusBoard(tk.Tk):
             # Frames all at same place --> move btwn them by raising in stack
             frame.grid(row=0, column=0, sticky="nsew")
         
+        #Start up the program on the start page
         self.show_frame("StartPage")
         
     # Show page (frame) associated with page_name
     def show_frame(self, page_name):
         frame = self.frames[page_name]
-        frame.tkraise()
-        
-# %% Start page class
-class StartPage(tk.Frame):
-    
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        
-        #Add objects in start page window
-        
-        
+        frame.tkraise()        
 
-# %% Executor
-masterApp = StatusBoard()
-masterApp.mainloop()
+# %% Execute Code
+if __name__ == "__main__":
+    master = StatusBoard()
+    master.mainloop()
---
+++ start_page.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Start page of the MUTR Status board
+
+Created on Wed Jun 24 16:34:55 2020
+@author: Telon J. Yan
+"""
+# %% Imports
+import tkinter as tk
+
+# %% Start page class
+class StartPage(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        
+        #Add objects in start page window
+        title_label = tk.Label(self, text="MUTR Core", font=controller.LARGE_FONT)
+        title_label.pack(side="top", fill="x", pady=10)
+        
+        button1 = tk.Button(self, text="Go to Test Page", font=controller.SMALL_FONT, 
+                            command=lambda: controller.show_frame("TestPage"))
+        button1.pack()
